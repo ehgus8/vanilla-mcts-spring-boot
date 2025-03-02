1f9181ce456b3a1ea46e89ae67b2e21e843cd5d7
ldh <68594605+ehgus8@users.noreply.github.com>
2025-03-02 20:09:46 +0900

    Initial commit

---
+++ src/main/java/com/example/vanilla_mcts/VanillaMctsApplication.java
@@ -0,0 +1,13 @@
+package com.example.vanilla_mcts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanillaMctsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanillaMctsApplication.class, args);
+	}
+
+}
